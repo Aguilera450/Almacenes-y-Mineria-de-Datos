623820fec1777a345b0b1209b9f832ef6606cc62
hdisra318 <hdisra318@ciencias.unam.mx>
2023-09-04 22:19:39 -0600

    Agregando primeras tres vistas

---
+++ Practica01/Script.sql
@@ -0,0 +1,36 @@
+/* Vista para la tabla empleado con el CURP y nombre completo */
+CREATE VIEW vista_empleados
+as select curp, nombre, paterno, materno 
+from Empleado
+WHERE nombre LIKE 'A%';
+
+/* Consultando vista_empleados */
+SELECT * FROM vista_empleados;
+
+
+/* Vista con toda la informacion de los empleados y departamentos */
+CREATE VIEW vista_empleados_departamentos
+as select e.curp, e.nombre, e.paterno, e.materno, e.salario,
+e.genero, e.nacimiento, e.ciudad, e.calle, e.cp, d.numDepto,
+d.nombreDepto, d.fecha
+from Empleado e 
+join Departamento d
+on e.numDepto=d.numDepto;
+
+/* Consultando vista_empleados_departamentos */
+SELECT * FROM vista_empleados_departamentos;
+
+
+/* Vista que muestra numProyecto, nombreProyecto, el numero de empleados y el
+numero de horas colaboradas de cada proyecto */
+CREATE VIEW vista_proyecto_empleados
+as select p.numProy, p.nombreProy, 
+count(c.curp) as empleadosEnProyecto,
+sum(c.numHoras) as horasColaboradas
+from Colaborar c
+join Proyecto p
+on c.numProy=p.numProy
+group by p.numProy, p.nombreProy;
+
+/* Consultando vista_proyecto_empleados */
+SELECT * FROM vista_proyecto_empleados;
