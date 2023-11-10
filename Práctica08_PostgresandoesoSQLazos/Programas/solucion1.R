7c945761f2744174ad3d7eb6ba7a02bbfab2d615
Marco Rivera Silva <marcoantonioriverasilva@ciencias.unam.mx>
2023-11-09 20:26:52 -0600

    Update solucion1.R

--- Práctica08_PostgresandoesoSQLazos/Programas/solucion1.R
+++ Práctica08_PostgresandoesoSQLazos/Programas/solucion1.R
@@ -8,6 +8,48 @@ summary(vgsales)
 head(vgsales)
 
 # ---- Limpieza de datos ----
+#Hacemos el csv limpio
+vgsalesLimpio <- vgsales
+
+# Ponemos las plataformas en mayusculas
+vgsalesLimpio$Platform <- toupper(vgsalesLimpio$Platform)
+
+# Corrigiendo los valores erroneos de WII
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "WI", "WII", vgsalesLimpio$Platform)
+
+# Corrigiendo los valores erroneos de Play Station 2
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "PLAYSTATION2", "PS2", vgsalesLimpio$Platform)
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "PSTATION2", "PS2", vgsalesLimpio$Platform)
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "PLAYS2", "PS2", vgsalesLimpio$Platform)
+
+# Unificando los nombres para las consolas de XBOX
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "XB", "XBOX", vgsalesLimpio$Platform)
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "XB360", "XBOX360", vgsalesLimpio$Platform)
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "X360", "XBOX360", vgsalesLimpio$Platform)
+vgsalesLimpio$Platform <- ifelse(vgsalesLimpio$Platform == "XONE", "XBOXONE", vgsalesLimpio$Platform)
+
+# Corrigiendo filas que tienen mal escrito Action
+vgsalesLimpio$Genre <- ifelse(vgsalesLimpio$Genre == "ACTON", "Action", vgsalesLimpio$Genre)
+vgsalesLimpio$Genre <- ifelse(vgsalesLimpio$Genre == "ation", "Action", vgsalesLimpio$Genre)
+
+# Corrigiendo filas que tienen genero en blanco por Misc
+vgsalesLimpio$Genre <- ifelse(vgsalesLimpio$Genre == "", "Misc", vgsalesLimpio$Genre)
+
+# Corrigiendo filas que tienen mal escrito Nintendo
+vgsalesLimpio$Publisher <- ifelse(vgsalesLimpio$Publisher == "intendo", "Nintendo", vgsalesLimpio$Publisher)
+vgsalesLimpio$Publisher <- ifelse(vgsalesLimpio$Publisher == "nintndo", "Nintendo", vgsalesLimpio$Publisher)
+
+# Unificando SNK PLaymore y SNK en solo SNK Playmore
+vgsalesLimpio$Publisher <- ifelse(vgsalesLimpio$Publisher == "SNK", "SNK PLAYMORE", vgsalesLimpio$Publisher)
+
+# Exportamos el csv
+write.csv(vgsalesLimpio, "vgsalesLimpio.csv", row.names = FALSE)
+
+#Prueba Chi
+
+tabla <- table(vgsalesLimpio$Genre, vgsalesLimpio$Publisher)
+
+chisq.test(tabla)
 
 # -- Valores faltantes --
 # Obteniendo la suma de los valores faltantes en toda la columna de los datos de "vgsales": 
