4bad3f1ce567cfca9c3f8657ca24c60aeabad52a
hdisra318 <hdisra318@ciencias.unam.mx>
2023-12-05 12:32:11 -0600

    Agregando moda a variables de tipo character

--- ProyectoFinal/prepDatosTerrorismo.R
+++ ProyectoFinal/prepDatosTerrorismo.R
@@ -15,18 +15,41 @@ variables_conservadas <- c("iyear", "imonth", "iday", "country_txt", "success", 
                            "crit1", "crit2", "crit3")
 globalTerr <- globalTerr %>% select(variables_conservadas)     
 globalTerr
+str(globalTerr)
 
 # Imputando los datos
 install.packages("mice")
 library(mice)
-globalTerr_inputados <- mice(globalTerr, method = "pmm", m = 5)
+# Colcando la media o mediana en las variables numericas
+globalTerr_inputados <- mice(globalTerr, method = "pmm", m = 5, seed = 500)
 globalTerr_inputados <- complete(globalTerr_inputados)
 str(globalTerr_inputados)
+globalTerr_inputados
+
+# Colocando la moda de las otras variables
+for (col in names(globalTerr_inputados)) {
+  if (class(globalTerr[[col]]) == "character" && sum(is.na(globalTerr[[col]])) > 0) {
+    globalTerr_inputados[[col]] <- factor(globalTerr_inputados[[col]])
+    globalTerr_inputados[[col]] <- as.character(imputeMode(globalTerr_inputados[[col]]))
+  }
+}
+
+globalTerr_inputados <- complete(globalTerr_inputados)
+str(globalTerr_inputados)
+globalTerr_inputados
 
 # Eliminando valores atipicos
+names(globalTerr_inputados)
+attach(globalTerr_inputados)
+table(country_txt)
 
 
-# Discretizando variables numericas
+
+# Discretizando valores numericos
 install.packages("funModeling")
 library(funModeling)
+
+
+# Guardando los datos preprocesados
+write.csv(globalTerr_inputados, file = "datosPrepTerrorismo.csv", row.names = FALSE)
 
